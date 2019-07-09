12e2c0b8f4f5c7f39ef186fd970d8cc0157a47da
Karthik Prahash.M <46716288+karthikprahash@users.noreply.github.com>
2019-07-09 18:54:12 +0530

    Update and rename README.md to factorial.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# factorial-of-the-number
---
+++ factorial.py
@@ -0,0 +1,12 @@
+# factorial-of-the-number
+i=int(input())
+if i!=0:
+    s=1
+    while(i>1):
+        s=i*s
+        i=i-1
+    print(s)
+else:
+    print('1')
+
+    
